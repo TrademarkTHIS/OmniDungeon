baedb88abe7b65118337e9d36be2a55f59ce55e3
Jacket <jaketvro@gmail.com>
2025-10-18 16:17:32 -0700

    Bounds now extends Rectangle

--- core/src/main/java/me/arcademadness/omni_dungeon/components/Bounds.java
+++ core/src/main/java/me/arcademadness/omni_dungeon/components/Bounds.java
@@ -1,18 +1,15 @@
 package me.arcademadness.omni_dungeon.components;
 
-public class Bounds {
-    public final float x, y, width, height;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Bounds extends Rectangle {
 
     public Bounds(float x, float y, float width, float height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+        this.set(x,y,width,height);
     }
 
     public boolean intersects(Bounds other) {
-        return x < other.x + other.width && x + width > other.x &&
-            y < other.y + other.height && y + height > other.y;
+        return this.overlaps(other);
     }
 }
 
